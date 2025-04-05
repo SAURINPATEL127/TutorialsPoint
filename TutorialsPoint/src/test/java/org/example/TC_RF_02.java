e38ed49eddcd7346184a95bcca03e37d44830f9f
SAURINPATEL127 <saurin11292@gmail.com>
2025-04-05 00:31:32 -0400

    testCase2commit

--- TutorialsPoint/src/test/java/org/example/TC_RF_02.java
+++ TutorialsPoint/src/test/java/org/example/TC_RF_02.java
@@ -9,19 +9,24 @@ import java.util.Date;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import java.util.Random;
-
+/*
+        This test case automates the registration form of Tutorialsninja website.
+        Generates Random Name, Phonenumber.
+        Used asserts to verify users are redirected to the appropriate web-pages.
+ */
 public class TC_RF_02 {
+
+    WebDriver driver = new ChromeDriver();
 
     @Test
     public void testCase2() throws InterruptedException
     {
-        WebDriver driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5)); // Global wait of 5 Seconds
         driver.manage().window().maximize();
         driver.get("https://www.tutorialsninja.com/demo/");
         driver.findElement(By.xpath("//span[text()='My Account']")).click();
         driver.findElement(By.linkText("Register")).click();
-        driver.findElement(By.id("input-firstname")).sendKeys("SAURIN");
+        driver.findElement(By.id("input-firstname")).sendKeys(generateRandomName());
         Thread.sleep(4000);
         driver.findElement(By.id("input-lastname")).sendKeys("Patel");
         driver.findElement(By.id("input-email")).sendKeys(generateRandomEmail());
@@ -35,8 +40,11 @@ public class TC_RF_02 {
         driver.findElement(By.linkText("Continue")).click();
         Assert.assertTrue(driver.findElement(By.linkText("Edit your account information")).isDisplayed());
 
+        cleanUp();
 
     }
+
+
 
     public String generateRandomEmail(){
 
@@ -53,4 +61,21 @@ public class TC_RF_02 {
         return phoneNumber.toString();
     }
 
+    public static String generateRandomName() {
+        String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        Random random = new Random();
+        StringBuilder randomName = new StringBuilder();
+        int nameLength = random.nextInt(5) + 5; // Random name length between 5 and 10 characters
+
+        for (int i = 0; i < nameLength; i++) {
+            int randomIndex = random.nextInt(characters.length());
+            randomName.append(characters.charAt(randomIndex));
+        }
+
+        return randomName.toString();
+    }
+
+    public void cleanUp(){
+        driver.quit();
+    }
 }
